ccaf8bbd4fa008f8d883d3c885b3e0cf8199c977
Aidahar <zagirik@gmail.com>
2022-01-20 19:51:10 +0300

    add routes

--- main.go
+++ main.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
+
 type Book struct {
 	ID     int    `json:"id"`
 	Title  string `json:"title"`
@@ -10,5 +16,33 @@ type Book struct {
 var books []Book
 
 func main() {
+	router := mux.NewRouter()
 
+	router.HandleFunc("/books", getBooks).Methods("GET")
+	router.HandleFunc("/books/{id:[0-9]+}", getBook).Methods("GET")
+	router.HandleFunc("/books", addBook).Methods("POST")
+	router.HandleFunc("/books", updateBook).Methods("PUT")
+	router.HandleFunc("/books/{id:[0-9]+}", deleteBook).Methods("DELETE")
+
+	log.Fatal(http.ListenAndServe(":8000", router))
+}
+
+func getBooks(w http.ResponseWriter, r *http.Request) {
+	log.Println("Gets all books")
+}
+
+func getBook(w http.ResponseWriter, r *http.Request) {
+	log.Println("Get one book")
+}
+
+func addBook(w http.ResponseWriter, r *http.Request) {
+	log.Println("Adds one book")
+}
+
+func updateBook(w http.ResponseWriter, r *http.Request) {
+	log.Println("Updates a book")
+}
+
+func deleteBook(w http.ResponseWriter, r *http.Request) {
+	log.Println("Delete a book")
 }
